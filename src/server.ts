e2faa25c85905cfecf40a23335ae181a5964511d
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2023-08-16 20:53:23 -0400

    refactor: :sparkles: express json suporte

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,48 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json())
 const PORT = 3000;
 
+// visivel no navegador é o método GET
 app.get('/hora',function(req, res) {
-  res.send('agora sao 19h47');
+  res.json({horaAtual:'agora sao 19h47'});
 })
+// POST
+
+app.post('/soma', (request,response)=>{
+  // const {val1,val2} = request.params
+  // const {q1,q2} = request.query
+  const {v1,v2} = request.body
+  console.log(v1,v2)
+  const somatorio = v1 + v2
+  response.json({resultado:somatorio})
+
+
+})
+// PUT
+app.put('')
+
+// DELETE
+app.delete('')
 
 app.listen(PORT,() => {console.log(`servidor rodando na porta = ${PORT}`,)})
+
+/**
+ * https://www.skyscanner.com.br
+ * /transporte
+ * /passagens-aereas
+ * /nat
+ * /saoa
+ * /230906
+ * /230913
+ * /?adultsv2=1&
+ * cabinclass=economy
+ * &childrenv2=
+ * &inboundaltsenabled=false
+ * &outboundaltsenabled=false
+ * &preferdirects=false
+ * &qp_prevScreen=HOMEPAGE
+ * &ref=home
+ * &rtn=1
+ */
